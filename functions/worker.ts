4c24b47fd80be12b1a1101dbfe1a03b94ad5a480
Tomasz Dysinski <dysinski.tomasz@gmail.com>
2024-11-26 22:23:27 -0800

    linting fixes

--- functions/worker.ts
+++ functions/worker.ts
@@ -1,6 +1,6 @@
-import { onRequest as endpointHandler } from './api/endpoint';
-import { onRequest as registerWebhookHandler } from './api/registerWebhook';
-import { onRequest as unregisterWebhookHandler } from './api/unregisterWebhook';
+import { onRequest as endpointHandler } from "./api/endpoint";
+import { onRequest as registerWebhookHandler } from "./api/registerWebhook";
+import { onRequest as unregisterWebhookHandler } from "./api/unregisterWebhook";
 
 export interface Env {
   BOT_TOKEN: string;
@@ -9,18 +9,19 @@ export interface Env {
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+    void ctx;
     const url = new URL(request.url);
-    
+
     // Route the request to the appropriate handler
     switch (url.pathname) {
-      case '/api/endpoint':
-        return endpointHandler({ request, env } as any);
-      case '/api/registerWebhook':
-        return registerWebhookHandler({ request, env } as any);
-      case '/api/unregisterWebhook':
-        return unregisterWebhookHandler({ request, env } as any);
+      case "/api/endpoint":
+        return endpointHandler({ request, env });
+      case "/api/registerWebhook":
+        return registerWebhookHandler({ request, env });
+      case "/api/unregisterWebhook":
+        return unregisterWebhookHandler({ request, env });
       default:
-        return new Response('Not found', { status: 404 });
+        return new Response("Not found", { status: 404 });
     }
   },
 };
